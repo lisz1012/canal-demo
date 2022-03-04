20bb7e492a8ac38a0b76060e0bfe931773a7ced8
lisz1012 <lisz1012@163.com>
2022-03-03 21:25:26 -0800

    Add notes

--- src/main/java/com/houge/canal/AppMain.java
+++ src/main/java/com/houge/canal/AppMain.java
@@ -15,6 +15,8 @@ import java.util.stream.Collectors;
 
 /*
  * 注意⚠️ 下面的 "canal" 和 "redis-1" 都是主机名，请用当前看环境中的主机名或者IP代替
+ * 程序启动后就定期地从canal中拉取数据，然后转换成一个JSON字符串，并以它为value，user_id为key，通过
+ * Jedis客户端写入Redis
  */
 public class AppMain {
     public static void main(String[] args) {
@@ -36,22 +38,20 @@ public class AppMain {
                     } else {
                         processEntries(message.getEntries(), jedis);
                     }
-
                     connector.ack(batchId); // 提交确认
                 } catch (Throwable t) {
                     connector.rollback(batchId); // 处理失败, 回滚数据
                 }
             }
-
         } finally {
             connector.disconnect();
-
         }
     }
 
     private static void processEntries(List<CanalEntry.Entry> entrys, Jedis jedis) {
         for (CanalEntry.Entry entry : entrys) {
-            if (entry.getEntryType() == CanalEntry.EntryType.TRANSACTIONBEGIN || entry.getEntryType() == CanalEntry.EntryType.TRANSACTIONEND) {
+            if (entry.getEntryType() == CanalEntry.EntryType.TRANSACTIONBEGIN ||
+                    entry.getEntryType() == CanalEntry.EntryType.TRANSACTIONEND) {
                 continue;
             }
 
@@ -75,13 +75,15 @@ public class AppMain {
                     jedis.del(row2Key("user_id", rowData.getBeforeColumnsList()));
                 } else if (eventType == CanalEntry.EventType.INSERT) { // 插入
                     printColumn(rowData.getAfterColumnsList());
-                    jedis.set(row2Key("user_id", rowData.getAfterColumnsList()), row2Value(rowData.getAfterColumnsList()));
+                    jedis.set(row2Key("user_id", rowData.getAfterColumnsList()),
+                              row2Value(rowData.getAfterColumnsList()));
                 } else { // 更新
                     System.out.println("before");
                     printColumn(rowData.getBeforeColumnsList());
                     System.out.println("after");
                     printColumn(rowData.getAfterColumnsList());
-                    jedis.set(row2Key("user_id", rowData.getAfterColumnsList()), row2Value(rowData.getAfterColumnsList()));
+                    jedis.set(row2Key("user_id", rowData.getAfterColumnsList()),
+                              row2Value(rowData.getAfterColumnsList()));
 
                 }
             }
@@ -89,7 +91,6 @@ public class AppMain {
     }
 
     private static void printColumn(List<CanalEntry.Column> columns) {
-
         for (CanalEntry.Column column : columns) {
             System.out.println(column.getName() + " : " + column.getValue() + "    update=" + column.getUpdated());
         }
